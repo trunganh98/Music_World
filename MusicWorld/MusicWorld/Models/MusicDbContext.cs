5eb25e609be313961112d99e7a9c040d78017f76
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-01-26 09:29:25 +0700

    .

---
+++ MusicWorld/MusicWorld/Models/MusicDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicWorld.Models 
+{
+    public class MusicDbContext : DbContext
+    {
+        public MusicDbContext(DbContextOptions<MusicDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artists> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MusicWorld/MusicWorld/Models/SeedData.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicWorld.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulate(IApplicationBuilder app)
+        {
+            MusicDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<MusicDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Songs.Any())
+            {
+                context.Songs.AddRange(
+                    new Song
+                    {
+                        Title = "",
+                        Artists = "",
+                        Author = "",
+                        UrlImage = ""
+                    });
+
+            }
+            if (!context.Artists.Any())
+            {
+                context.Artists.AddRange(
+                    new Artists
+                    {
+                        Name = "",
+                        Birthday = "",
+                        Gender = "",
+                        Contact = "",
+                        UrlImage = ""
+                    });
+            }
+            if (!context.Albums.Any())
+            {
+                context.Albums.AddRange(
+                    new Album
+                    {
+                        Title = "",
+                        Description = "",
+                        Artists = "",
+                        Place = "",
+                        StartingDate = "",
+                        UrlImage = ""
+                    });
+            }
+            if (!context.Events.Any())
+            {
+                context.Events.AddRange(
+                    new Event
+                    {
+                        Title = "",
+                        Description = "",
+                        Artists = "",
+                        TicketPrice = "",
+                        Place = "",
+                        StartingDate = "",
+                        UrlImage = ""
+                    });
+            }
+        }
+    }
+   
+}

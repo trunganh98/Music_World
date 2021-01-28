373e8d4d122ccc5c3c32cdd1ae7ff4bf21e107c0
trunganh98 <trunganh98@users.noreply.github.com>
2021-01-28 09:53:58 +0700

    Update Login

--- MusicWorld/MusicWorld/Controllers/HomeController.cs
+++ MusicWorld/MusicWorld/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace MusicWorld.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
